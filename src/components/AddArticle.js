b9f419f9673fdd9010e101c17ae97713c44f085b
rangatron <aleop213@gmail.com>
2021-04-27 19:51:37 +1200

    made the code work so that it can now add articles

---
+++ src/components/AddArticle.js
@@ -0,0 +1,204 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+import axios from 'axios';
+
+
+class CreateBook extends Component {
+  constructor() {
+    super();
+    this.state = {
+      author: '',
+      title:'',
+      journal:'',
+      year:'',
+      eprint:'',
+      eprinttype:'',
+      pages:'',
+      month:'',
+      annote:''
+    };
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const data = {
+      author: this.state.author,
+      title: this.state.title,
+      journal: this.state.journal,
+      year: this.state.year,
+      eprinttype: this.state.eprint,
+      eprintclass: this.state.publisher,
+      pages: this.state.pages,
+      month: this.state.month,
+      annote: this.state.annote
+    };
+
+    axios
+      .post('http://localhost:8082/api/books', data)
+      .then(res => {
+        this.setState({
+          author: '',
+          title:'',
+          journal:'',
+          year:'',
+          eprint:'',
+          eprinttype:'',
+          pages:'',
+          month:'',
+          annote:''
+        })
+        this.props.history.push('/');
+      })
+      .catch(err => {
+        console.log("Error in CreateBook!");
+      })
+  };
+
+  render() {
+    return (
+      <div className="CreateBook">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <br />
+              <Link to="/" className="btn btn-outline-warning float-left">
+                  Show BooK List
+              </Link>
+            </div>
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Add Book</h1>
+              <p className="lead text-center">
+                  Create new book
+              </p>
+
+              <form noValidate onSubmit={this.onSubmit}>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Author of Article'
+                    name='author'
+                    className='form-control'
+                    value={this.state.author}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <br />
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Title'
+                    name='title'
+                    className='form-control'
+                    value={this.state.title}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Journal'
+                    name='journal'
+                    className='form-control'
+                    value={this.state.journal}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Year'
+                    name='year'
+                    className='form-control'
+                    value={this.state.year}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='eprint'
+                    name='eprint'
+                    className='form-control'
+                    value={this.state.eprint}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='eprinttype'
+                    name='eprinttype'
+                    className='form-control'
+                    value={this.state.eprinttype}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='eprintclass'
+                    name='eprintclass'
+                    className='form-control'
+                    value={this.state.eprintclass}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Pages'
+                    name='pages'
+                    className='form-control'
+                    value={this.state.pages}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='Month'
+                    name='month'
+                    className='form-control'
+                    value={this.state.month}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    placeholder='annote'
+                    name='annote'
+                    className='form-control'
+                    value={this.state.annote}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <input
+                    type="submit"
+                    className="btn btn-outline-warning btn-block mt-4"
+                />
+              </form>
+          </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CreateBook;
